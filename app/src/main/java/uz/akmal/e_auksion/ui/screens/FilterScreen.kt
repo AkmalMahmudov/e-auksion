b7cecb6e7fbf6a31d12d87b52633b0a678765cfd
Mahmudov <m.akmaljon2001@gmail.com>
2022-02-17 11:18:50 +0500

    commit

--- app/src/main/java/uz/akmal/e_auksion/ui/screens/FilterScreen.kt
+++ app/src/main/java/uz/akmal/e_auksion/ui/screens/FilterScreen.kt
@@ -1,7 +1,11 @@
 package uz.akmal.e_auksion.ui.screens
 
+import android.database.DataSetObserver
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SpinnerAdapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
@@ -17,6 +21,7 @@ class FilterScreen : Fragment(R.layout.fragment_filter) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
+            
             back.setOnClickListener {
                 navController.navigateUp()
             }

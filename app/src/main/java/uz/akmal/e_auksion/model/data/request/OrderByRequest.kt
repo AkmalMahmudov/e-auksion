5749ea5c39a30d0822f39a7505823d4e9d80277d
Mahmudov <m.akmaljon2001@gmail.com>
2022-02-17 11:35:13 +0500

    commit

--- app/src/main/java/uz/akmal/e_auksion/model/data/request/OrderByRequest.kt
+++ app/src/main/java/uz/akmal/e_auksion/model/data/request/OrderByRequest.kt
@@ -3,7 +3,7 @@ package uz.akmal.e_auksion.model.data.request
 data class OrderByRequest(
     val action: Int = 5,
     val currentPage: String = "1",
-    val orderBy_map: OrderByMap,
+    val filters_map: OrderByMap,
     val is_gzipped: Int = 0,
     val language: String = "uz",
     val version: String = "1.3.5"

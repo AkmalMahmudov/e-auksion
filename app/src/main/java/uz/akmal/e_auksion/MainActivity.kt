0e7c9366513f93d5ed59da93f12024dab641762b
Mahmudov <m.akmaljon2001@gmail.com>
2022-03-20 12:59:48 +0500

    commit

--- app/src/main/java/uz/akmal/e_auksion/MainActivity.kt
+++ app/src/main/java/uz/akmal/e_auksion/MainActivity.kt
@@ -2,12 +2,31 @@ package uz.akmal.e_auksion
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import uz.akmal.e_auksion.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private val binding by viewBinding(ActivityMainBinding::bind)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        drawerLayout = binding.drawerLayout
+        binding.navDrawer.setupWithNavController(navController)
+    }
+    fun openDrawer() {
+        if (binding.drawerLayout.isOpen) {
+            binding.drawerLayout.close()
+        } else {
+            binding.drawerLayout.open()
+        }
     }
 }
